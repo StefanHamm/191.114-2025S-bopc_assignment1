b661607af192d4490e67e184df41a391b60fd0dd
Philipp Wörndle <philipp.woerndle@siemens.com>
2025-04-05 11:47:51 +0200

    test

--- julia_set/julia_par.py
+++ julia_set/julia_par.py
@@ -8,18 +8,19 @@ from multiprocessing import Pool, TimeoutError
 from julia_curve import c_from_group
 
 # Update according to your group size and number (see TUWEL)
-GROUP_SIZE   = 3
+GROUP_SIZE = 3
 GROUP_NUMBER = 2
 
 # do not modify BENCHMARK_C
 BENCHMARK_C = complex(-0.2, -0.65)
+
 
 def compute_julia_set_sequential(xmin, xmax, ymin, ymax, im_width, im_height, c):
 
     zabs_max = 10
     nit_max = 300
 
-    xwidth  = xmax - xmin
+    xwidth = xmax - xmin
     yheight = ymax - ymin
 
     julia = np.zeros((im_width, im_height))
@@ -34,26 +35,28 @@ def compute_julia_set_sequential(xmin, xmax, ymin, ymax, im_width, im_height, c)
                 z = z**2 + c
                 nit += 1
             ratio = nit / nit_max
-            julia[ix,iy] = ratio
+            julia[ix, iy] = ratio
 
     return julia
+
 
 def compute_julia_in_parallel(size, xmin, xmax, ymin, ymax, patch, nprocs, c):
 
     pool = Pool(processorts=nprocs)
-    # assuming size (mod patch) = 0
-	for x in range(0, size, patch):
-		for y in range(0, size, patch):
-			task_list.append( (x, y, patch, meta_information) )
-	# meta information may contain offsets, original image size,
-	# original boundaries of complex plane, etc.
-	create Pool with nprocs workers
-	# make sure that each task in the task_list is handled alone
-	# in multiprocessing.Pool.map, we need to specify chunksize=1
-	completed_patches = Pool.map(compute_patch, task_list, 1)
-    for p in completed_patches:
-    copy subimage of p to correct final position
-    julia_img = compute_julia_set_sequential(xmin, xmax, ymin, ymax, size, size, c)
+    # # assuming size (mod patch) = 0
+    #    for x in range(0, size, patch):
+    #         for y in range(0, size, patch):
+    #             task_list.append((x, y, patch, meta_information))
+    #     # meta information may contain offsets, original image size,
+    #     # original boundaries of complex plane, etc.
+    #     create Pool with nprocs workers
+    #     # make sure that each task in the task_list is handled alone
+    #     # in multiprocessing.Pool.map, we need to specify chunksize=1
+    #     completed_patches = Pool.map(compute_patch, task_list, 1)
+    # for p in completed_patches:
+    # copy subimage of p to correct final position
+    julia_img = compute_julia_set_sequential(
+        xmin, xmax, ymin, ymax, size, size, c)
 
     return julia_img
 
@@ -61,21 +64,26 @@ def compute_julia_in_parallel(size, xmin, xmax, ymin, ymax, patch, nprocs, c):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--size", help="image size in pixels (square images)", type=int, default=500)
+    parser.add_argument(
+        "--size", help="image size in pixels (square images)", type=int, default=500)
     parser.add_argument("--xmin", help="", type=float, default=-1.5)
     parser.add_argument("--xmax", help="", type=float, default=1.5)
     parser.add_argument("--ymin", help="", type=float, default=-1.5)
     parser.add_argument("--ymax", help="", type=float, default=1.5)
     parser.add_argument("--group-size", help="", type=int, default=None)
     parser.add_argument("--group-number", help="", type=int, default=None)
-    parser.add_argument("--patch", help="patch size in pixels (square images)", type=int, default=20)
-    parser.add_argument("--nprocs", help="number of workers", type=int, default=1)
-    parser.add_argument("--draw-axes", help="Whether to draw axes", action="store_true")
+    parser.add_argument(
+        "--patch", help="patch size in pixels (square images)", type=int, default=20)
+    parser.add_argument(
+        "--nprocs", help="number of workers", type=int, default=1)
+    parser.add_argument(
+        "--draw-axes", help="Whether to draw axes", action="store_true")
     parser.add_argument("-o", help="output file")
-    parser.add_argument("--benchmark", help="Whether to execute the script with the benchmark Julia set", action="store_true")
+    parser.add_argument(
+        "--benchmark", help="Whether to execute the script with the benchmark Julia set", action="store_true")
     args = parser.parse_args()
 
-    #print(args)
+    # print(args)
     if args.group_size is not None:
         GROUP_SIZE = args.group_size
     if args.group_number is not None:
@@ -84,15 +92,15 @@ if __name__ == "__main__":
     # assign c based on mode
     c = None
     if args.benchmark:
-        c = BENCHMARK_C 
+        c = BENCHMARK_C
     else:
-        c = c_from_group(GROUP_SIZE, GROUP_NUMBER) 
+        c = c_from_group(GROUP_SIZE, GROUP_NUMBER)
 
     stime = time.perf_counter()
     julia_img = compute_julia_in_parallel(
         args.size,
-        args.xmin, args.xmax, 
-        args.ymin, args.ymax, 
+        args.xmin, args.xmax,
+        args.ymin, args.ymax,
         args.patch,
         args.nprocs,
         c)
@@ -121,16 +129,19 @@ if __name__ == "__main__":
 
             xtick_labels = np.linspace(xmin, xmax, 7)
             ax.set_xticks([(x-xmin) / xwidth * im_width for x in xtick_labels])
-            ax.set_xticklabels(['{:.1f}'.format(xtick) for xtick in xtick_labels])
+            ax.set_xticklabels(['{:.1f}'.format(xtick)
+                               for xtick in xtick_labels])
             ytick_labels = np.linspace(ymin, ymax, 7)
-            ax.set_yticks([(y-ymin) / yheight * im_height for y in ytick_labels])
-            ax.set_yticklabels(['{:.1f}'.format(-ytick) for ytick in ytick_labels])
+            ax.set_yticks(
+                [(y-ymin) / yheight * im_height for y in ytick_labels])
+            ax.set_yticklabels(['{:.1f}'.format(-ytick)
+                               for ytick in ytick_labels])
             ax.set_xlabel("Imag")
             ax.set_ylabel("Real")
         else:
             # disable axes
-            ax.axis("off") 
+            ax.axis("off")
 
         plt.tight_layout()
         plt.savefig(args.o, bbox_inches='tight')
-        #plt.show()
+        # plt.show()
